e0f9003ec07cd47c76199d996fdae6e783f283de
Boku <Boku@NHs-MacBook-Pro.local>
2018-06-14 16:21:04 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,33 @@
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
-alert("holy moley!");
+
+// console.log("User flipped " + cardOne);
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay[0]);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} else { 
+alert("Sorry, try again");
+}
+
+// Just a little experimentation
+/* if (cardsInPlay.length === 2) {	
+} if else (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again");
+}
+*/
+
+
+
