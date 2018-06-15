2de9bc86443ca07ec0f326350db4895f913b9225
Boku <Boku@NHs-MacBook-Pro.local>
2018-06-15 17:43:58 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [ 
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png",
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png",
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png",	
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds",
+}
+];
 
 var cardsInPlay = [];
 
@@ -11,11 +32,14 @@ alert("Sorry, try again.");
 };
 
 var flipCard = function(cardId) {
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
-if (cardsInPlay.length === 2) {	
+console.log("User flipped " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].suit);
+console.log(cards[cardId].cardImage);
+if (cardsInPlay.length === 2) 
+{	
 checkForMatch();
-	}
+}
 };
 	
 
